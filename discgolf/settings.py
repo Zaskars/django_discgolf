f5fe2285a7bb0436cd0183153ed4d5e2ee205988
Timofey Bolotov <timbolotov3@gmail.com>
2023-11-30 19:11:34 +0300

    +crispy forms, bootstrap

--- discgolf/settings.py
+++ discgolf/settings.py
@@ -39,7 +39,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "main",
+    "crispy_forms",
+    "crispy_bootstrap4",
 ]
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
