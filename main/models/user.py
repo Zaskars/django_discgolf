e5f29b6cd8f518c42df03f9cd29c443b21506acf
Timofey Bolotov <timbolotov3@gmail.com>
2023-11-30 23:28:26 +0300

    minor

--- main/models/user.py
+++ main/models/user.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class PlayerProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     rating = models.IntegerField(default=0)
-    subscription_status = models.BooleanField(
-        default=False
-    )  # True если пользователь с подпиской
+    subscription_status = models.BooleanField(default=False)
     city = models.CharField(max_length=100)
     birth_year = models.IntegerField()
 
