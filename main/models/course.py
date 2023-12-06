87133db772c77489cd157d6a973a9b52d650f42b
Timofey Bolotov <timbolotov3@gmail.com>
2023-12-06 20:23:56 +0300

    no more layout-basket. we got segment!

--- main/models/course.py
+++ main/models/course.py
@@ -21,20 +21,16 @@ class Layout(models.Model):
 class Basket(models.Model):
     course = models.ForeignKey(Course, related_name="baskets", on_delete=models.CASCADE)
     basket_number = models.IntegerField()
-    par = models.IntegerField()
 
     def __str__(self):
         return f"Basket {self.basket_number} of {self.course.name}"
 
 
-class LayoutBasket(models.Model):
-    layout = models.ForeignKey(
-        Layout, related_name="layout_baskets", on_delete=models.CASCADE
-    )
+class Segment(models.Model):
+    layout = models.ForeignKey(Layout, related_name="tees", on_delete=models.CASCADE)
     basket = models.ForeignKey(Basket, on_delete=models.CASCADE)
-
-    class Meta:
-        unique_together = ["layout", "basket"]
+    tee_number = models.IntegerField()
+    par = models.IntegerField()
 
     def __str__(self):
-        return f"{self.layout.name} - {self.basket}"
+        return f"Tee {self.tee_number} for {self.basket} in {self.layout.name}"
