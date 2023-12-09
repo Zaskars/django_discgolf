c2e6d1766f99d696023e06bbb9367db9ccc329ba
Timofey Bolotov <timbolotov3@gmail.com>
2023-12-10 02:01:50 +0300

    +layout security

--- main/views/layout.py
+++ main/views/layout.py
@@ -1,10 +1,18 @@
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import get_object_or_404
 from django.views.generic import ListView
-from main.models import Layout
+from main.models import Layout, Tournament
 
 
 class LayoutSelectionView(ListView):
     model = Layout
     template_name = "layout_selection.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        self.tournament = get_object_or_404(Tournament, pk=kwargs["tournament_id"])
+        if self.tournament.director != self.request.user:
+            raise PermissionDenied("Вы не имеете права редактировать этот турнир")
+        return super().dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
